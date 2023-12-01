611455f6be55ed8361bff62756870737742b9e80
Jeroen Vlek <jeroen@perceptivebits.com>
2023-12-01 20:16:52 +0100

    add candle tutorial snippet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use candle_core::{Device, Tensor};
+
 fn main() {
-    println!("Hello, Julia!");
+    let data: [u32; 3] = [1u32, 2, 3];
+    let tensor = Tensor::new(&data, &Device::Cpu).unwrap();
+    println!("tensor: {:?}", tensor.to_vec1::<u32>().unwrap());
+
+    let nested_data: [[u32; 3]; 3] = [[1u32, 2, 3], [4, 5, 6], [7, 8, 9]];
+    let nested_tensor = Tensor::new(&nested_data, &Device::Cpu).unwrap();
+    println!("nested_tensor: {:?}", nested_tensor.to_vec2::<u32>().unwrap());
 }
+
+
